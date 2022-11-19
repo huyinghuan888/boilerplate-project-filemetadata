d160f1f330de489f8e0936abaf1845a2e6dfd6f7
huyinghuan <huyinghuan@qq.com>
2022-11-20 02:16:56 +0800

    初步完成

--- index.js
+++ index.js
@@ -1,20 +1,36 @@
-var express = require('express');
-var cors = require('cors');
-require('dotenv').config()
+import dotenv from 'dotenv';
+import express from 'express';
+import cors from 'cors';
+import multer from 'multer';
 
-var app = express();
+dotenv.config();
+const app = express();
+const upload = multer();
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
 
+/**
+ * 应用首页
+ */
 app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
+/**
+ * 单文件上传
+ */
+app.post('/api/fileanalyse', upload.single('upfile'), (req, res) => {
+  const name = req.file.originalName;
+  const type = req.file.clientReportedMimeType;
+  const size = req.file.size;
 
+  if (name === '') return res.json({ error: '未选择文件！' });
 
+  res.json({ name, type, size });
+});
 
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
-  console.log('Your app is listening on port ' + port)
+  console.log('Your app is listening on port ' + port);
 });
